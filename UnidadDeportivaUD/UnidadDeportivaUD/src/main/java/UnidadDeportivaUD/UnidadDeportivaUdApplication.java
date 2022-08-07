d03ad6c91120753ff9326e4ab68a3930671f4e5c
Laura Sofia Dueñas Bulla <lauraduenas1105@gmail.com>
2022-08-07 18:54:48 -0500

    entidades

---
+++ UnidadDeportivaUD/UnidadDeportivaUD/src/main/java/UnidadDeportivaUD/UnidadDeportivaUdApplication.java
@@ -0,0 +1,13 @@
+package UnidadDeportivaUD;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class UnidadDeportivaUdApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(UnidadDeportivaUdApplication.class, args);
+	}
+
+}
---
+++ UnidadDeportivaUD/UnidadDeportivaUD/src/test/java/UnidadDeportivaUD/UnidadDeportivaUdApplicationTests.java
@@ -0,0 +1,13 @@
+package UnidadDeportivaUD;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class UnidadDeportivaUdApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
