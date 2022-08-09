beafab949221b3a538865640c6696afbbd516f9d
Laura Sofia Dueñas Bulla <lauraduenas1105@gmail.com>
2022-08-08 19:48:33 -0500

    fix

--- UnidadDeportivaUD/UnidadDeportivaUD/src/main/java/UnidadDeportivaUD/repository/PrestamoRepository.java
+++
@@ -1,9 +0,0 @@
-package UnidadDeportivaUD.repository;
-
-import UnidadDeportivaUD.model.Prestamo;
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface PrestamoRepository extends JpaRepository<Prestamo, Long> {
-}
